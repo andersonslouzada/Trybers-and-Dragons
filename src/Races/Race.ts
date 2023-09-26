f9a391deeaff58c7d09017d4198d5dd81990a607
andersonlouzada <anderson.s.louzada@gmail.com>
2023-09-26 10:33:05 -0300

    feat: Cria classe Race e o arquivo index.ts e cumpre requisito 01

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+abstract class Race { 
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  static createdRacesInstances() {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+  
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+}
+  
+export default Race;
