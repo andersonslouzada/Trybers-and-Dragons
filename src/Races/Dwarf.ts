4f8c3458f2d841d24b9f26ef18555026dc1f7120
andersonlouzada <anderson.s.louzada@gmail.com>
2023-09-26 10:43:35 -0300

    feat: cria class Dwarf

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+export default class Dwarf extends Race {
+  protected _maxLifePoints: number;
+  protected static _createdInstances = 0;
+
+  constructor(name: string, dexterity: number, maxLifePoints = 80) {
+    super(name, dexterity);
+    this._maxLifePoints = maxLifePoints;
+    Dwarf._createdInstances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Dwarf._createdInstances;
+  }
+}
